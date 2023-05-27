a8fb4e48c6c9f254de1542b9c5d497c073d7220e
Abdusattor <Abdusator87@gmail.com>
2023-05-27 09:25:17 +0500

    add swagger

--- menu/urls.py
+++ menu/urls.py
@@ -30,8 +30,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns += [
     # ...
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
--- menu_app/models/product.py
+++ menu_app/models/product.py
@@ -5,7 +5,7 @@ class  Product(models.Model):
     name =models.CharField("Блюдо",max_length=70,null=False) 
     category=models.ForeignKey("menu_app.category",on_delete=models.CASCADE,related_name="categories",verbose_name="Категория",null=False)
     image = models.ImageField(upload_to="Photos/")
-    price =models.CharField("Цена",max_length=10) 
+    price =models.DecimalField("Цена",max_length=10,max_digits=6,decimal_places=2) 
     compound=models.ManyToManyField("menu_app.compound" ,related_name="compounds",verbose_name="Состав")
 
 
